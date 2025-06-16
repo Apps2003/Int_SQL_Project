1c6d690a8b26ee86e974a9312ef46b6a3db70e0b
Apps <aparnarane2003@gmail.com>
2025-06-16 14:00:09 +0530

    Add files via upload

---
+++ 2_cohort_analysis.sql
@@ -0,0 +1,26 @@
+DROP VIEW public.cohort_analysis;
+
+CREATE OR REPLACE VIEW public.cohort_analysis
+AS WITH customer_revenue AS (
+         SELECT s.customerkey,
+            s.orderdate,
+            sum(s.quantity::double precision * s.netprice / s.exchangerate) AS total_net_revenue,
+            count(s.orderkey) AS num_orders,
+            c.countryfull,
+            c.age,
+            c.givenname,
+            c.surname
+           FROM sales s
+             LEFT JOIN customer c ON c.customerkey = s.customerkey
+          GROUP BY c.countryfull, c.age, c.givenname, c.surname, s.customerkey, s.orderdate
+        )
+ SELECT customerkey,
+    orderdate,
+    total_net_revenue,
+    num_orders,
+    countryfull,
+    age,
+    CONCAT(TRIM(givenname), ' ', TRIM(surname)) AS fullname,
+    min(orderdate) OVER (PARTITION BY customerkey) AS first_purchase_date,
+    EXTRACT(year FROM min(orderdate) OVER (PARTITION BY customerkey)) AS cohort_year
+   FROM customer_revenue cr;
